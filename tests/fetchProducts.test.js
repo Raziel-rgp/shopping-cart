7ed4b0fee55b5517412fa898e7117cc72d7e1159
fawl_03 <razidias@gmail.com>
2022-07-11 14:24:22 -0300

    att 1.1.0

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,7 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('test se o typeof de  é uma funcao', () => {
+    expect(typeof(fetchProducts)).toEqual('function');
+  })
 });
