0a6f2c4b40474db90fe9b59cf1fc351522702a6a
fawl_03 <razidias@gmail.com>
2022-07-21 16:15:39 -0300

    att 1.1.3

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (produtoName) => {
+  const linkbrabo = `https://api.mercadolibre.com/sites/MLB/search?q=${produtoName}`;
+  const fetch2 = await fetch(linkbrabo);
+  const slaOq = await fetch2.json();
+  return slaOq;
 };
+fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,6 +4,18 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   it('test se o typeof de  é uma funcao', () => {
-    expect(typeof(fetchProducts)).toEqual('function');
-  })
+    expect(typeof fetchProducts).toEqual('function');
+  });
+  it('teste se a funcao é chamada quando passado um valor chamado computador', () => {
+    fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('teste se a funcao é chamada quando passado um valor chamado computador o link esta correto', async () => {
+    const nameProduct = 'computador'
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${nameProduct}`
+    await fetchProducts(nameProduct)
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(url);
+    });
+  
 });
