0633a351049069fd9c9c2b901a52eb867539dc2f
fawl_03 <razidias@gmail.com>
2022-07-22 02:34:48 -0300

    att 1.2.4 2 concluido(talvez)

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,16 +1,8 @@
 const fetchProducts = async (produtoName) => {
-  try {
-    if (!produtoName) {
-      throw new Error('You must provide an url');
-    } 
-    const linkbrabo = `https://api.mercadolibre.com/sites/MLB/search?q=${produtoName}`;
-    const promice = await fetch(linkbrabo);
-    const produtos = await promice.json();
-    return produtos;
-  } catch (err) {
-    console.log('Deu erro familia');
-    throw err;
-  }
+  const linkbrabo = `https://api.mercadolibre.com/sites/MLB/search?q=${produtoName}`;
+  const promice = await fetch(linkbrabo);
+  const produtos = await promice.json();
+  return produtos;
 };
 if (typeof module !== 'undefined') {
   module.exports = {
