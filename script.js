df5aa61b9effc663b7480913a1b35b52bd6c0e4c
fawl_03 <razidias@gmail.com>
2022-07-24 17:24:24 -0300

    1.4.7 concluí 4, tinha feito o 12 achando que era o 4

--- script.js
+++ script.js
@@ -36,10 +36,17 @@ const products = async () => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = async () => {
+const cartCleaner = async () => {
   const elemento = document.querySelector('.cart__items');
   while (elemento.firstChild) {
   elemento.removeChild(elemento.firstChild);
+}
+};
+
+const cartItemCleaner = async () => {
+  const no = document.querySelector('.cart__item');
+if (no.parentNode) {
+  no.parentNode.removeChild(no);
 }
 };
 
@@ -47,7 +54,7 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemCleaner);
   return li;
 };
 const searchItemById = async (event) => {  
@@ -60,7 +67,7 @@ const getItem = async () => {
   const procurador = document.querySelectorAll('.item__add');
   procurador.forEach((item) => item.addEventListener('click', searchItemById));
   const botaoCart = document.querySelector('.empty-cart');
-  botaoCart.addEventListener('click', cartItemClickListener);
+  botaoCart.addEventListener('click', cartCleaner);
 };
 
 window.onload = async () => {
